d1463ce5efc9a91e47ac0d465d70e6a1a7827b9c
Jhon Heldher <jhonheldher48@gmail.com>
2023-04-19 17:44:39 -0300

    v3.0
    Fix bug

--- app.js
+++ app.js
@@ -34,73 +34,118 @@ function newTask() {
       name: input.value,
     });
     localStorage.setItem(localStorageKey, JSON.stringify(values));
+    
+    input.value = '';
     showValues();
-  }
-  input.value = '';
+  } 
+  
 }
 /* ========================================================================== */
 function searchItems() {
   let searchTerm = document.getElementById('search-input').value; // Obtém o valor do termo de pesquisa
   let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]"); // Obtém os itens do localStorage
   let list = document.getElementById('to-do-list');
-  list.innerHTML = '';
   
   // Filtra os itens com base no termo de pesquisa
   let filteredValues = values.filter(item => item['name'].toLowerCase().includes(searchTerm.toLowerCase()));
+  
+  // Limpa a lista antes de exibir os resultados
+  list.innerHTML = '';
   
   for (let i = 0; i < filteredValues.length; i++) {
     list.innerHTML += `<li>${filteredValues[i]['name']}
       <button id="btc-trash" onclick='removeItemDef("${filteredValues[i]['name']}")'>
       </button></li>`;
   }
+  
+  // Se não houver resultados, exibe uma mensagem de aviso
+  if (filteredValues.length === 0) {
+    list.innerHTML = '<li>Nenhum resultado encontrado</li>';
+  }
 }
+document.getElementById('search-input').addEventListener('input', searchItems);
+
 /* ========================================================================== */
-function showValues(){
-	let values =  JSON.parse(localStorage.getItem(localStorageKey) || "[]")
-	let list = document.getElementById('to-do-list')
-	list.innerHTML = ''
-	for(let i = 0; i < values.length; i++){
-		list.innerHTML += 
-    `<li>
-    <div id="btn-star">
-      <label id="ch">
-        <input id="check" type="checkbox" onchange='updateCheckbox("${values[i]['name']}", this.checked)' ${values[i]['checked'] ? 'checked' : ''}>
-        <span class="checkbox"></span>
-      </label>
-      ${values[i]['name']} 
-    </div>
-    <div id="btn-fim">
-      <button id='btn-edit' onclick='editItem("${values[i]['name']}")'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-          <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-        </svg>
-      </button>
-      <button id='btn-excluir' onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-        </svg>
-      </button>
-    </div></li>` 
-	}
-  let allOption = document.getElementById('all-option');
-  allOption.classList.add('clicado');
+
+function showValues() {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let list = document.getElementById('to-do-list');
+  let allBadge = document.getElementById('all-badge');
+  let doneBadge = document.getElementById('done-badge'); // Obter o elemento do badge
+  
+  // Remover classe de cor dos elementos
+  document.getElementById('click-done').classList.remove('option-clicked');
+  document.getElementById('click-trash').classList.remove('option-clicked');
+  
+  // Adicionar classe de cor ao elemento desejado
+  document.getElementById('click-all').classList.add('option-clicked');
+  
+  list.innerHTML = '';
+  let count = 0; // Contador para itens concluídos
+  for (let i = 0; i < values.length; i++) {
+    list.innerHTML += `
+      <li>
+        <div id="btn-star">
+          <label id="ch">
+            <input id="check" title="Clique para concluir Task" type="checkbox" onchange='updateCheckbox("${values[i]['name']}", this.checked)' ${values[i]['checked'] ? 'checked' : ''}>
+            <span class="checkbox"></span>
+          </label>
+          ${values[i]['name']} 
+        </div>
+        <div id="btn-fim">
+          <button id='btn-edit' title="Editar Task" onclick='editItem("${values[i]['name']}")'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+              <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+            </svg>
+          </button>
+          <button id='btn-excluir' title="Mover para lixeira" onclick='removeItem("${values[i]['name']}")'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+            </svg>
+            <span class="badge" id="trash-badge"></span>
+          </button>
+        </div>
+      </li>`;
+    
+    if (values[i]['checked']) { // Verifica se o item foi marcado como checado
+      count++; // Incrementa o contador de itens concluídos
+    }
+  }
+  
+  allBadge.textContent = values.length;
+  doneBadge.textContent = count; // Atualiza o valor do badge done-badge
 }
+
+
+
 /* ========================================================================== */
+
 function showValuesTrash() {
   let valuesTrash = JSON.parse(localStorage.getItem(localStorageKeyTrash) || "[]");
   let list = document.getElementById('to-do-list');
+  let trashBadge = document.getElementById('trash-badge'); // Obter o elemento do badge
+  
+  // Remover classe de cor dos elementos
+  document.getElementById('click-all').classList.remove('option-clicked');
+  document.getElementById('click-done').classList.remove('option-clicked');
+
+  // Adicionar classe de cor ao elemento desejado
+  document.getElementById('click-trash').classList.add('option-clicked');
+
+
   list.innerHTML = '';
   for (let i = 0; i < valuesTrash.length; i++) {
     list.innerHTML += `<li>${valuesTrash[i]['name']}
-      <button id="btc-trash" onclick='removeItemDef("${valuesTrash[i]['name']}")'>
+      <button id="btc-trash" title="Excluir definitivamente  a Task" onclick='removeItemDef("${valuesTrash[i]['name']}")'>
         <svg id="op" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
         </svg>
       </button></li>`;
-      let OptionDone = document.getElementById('click-done');
   }
+  trashBadge.textContent = valuesTrash.length; // Atualizar o valor do badge com a contagem de itens na lixeira
 }
 function pushTrash() {
   showValuesTrash();
@@ -109,19 +154,31 @@ function pushTrash() {
 function showValuesDone() {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
   let list = document.getElementById('to-do-list');
+  let doneBadge = document.getElementById('done-badge');
+  
+  document.getElementById('click-all').classList.remove('option-clicked');
+  document.getElementById('click-trash').classList.remove('option-clicked');
+  document.getElementById('click-done').classList.add('option-clicked');
+  let count = 0;
   list.innerHTML = '';
   for (let i = 0; i < values.length; i++) {
-    if (values[i]['checked']) { // Verifica se o item foi marcado como checado
+    if (values[i]['checked']) {
       list.innerHTML += `<li>${values[i]['name']}
-        <button id="btc-trash" onclick='removeItem("${values[i]['name']}")'>
+        <button id="btc-trash" title="Mover para lixeira" onclick='removeItem("${values[i]['name']}")'>
         <svg id="op" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
         </svg>
-        </button></li>`;
+        </button></li>`
+        count++;
     }
   }
+  doneBadge.textContent = count;
+  
+  // Atualizar o badge de concluídos
+  doneBadge.textContent = count;
 }
+
 /* ========================================================================== */
 function editItem(data) {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
@@ -165,9 +222,14 @@ function removeItem(data) {
   valuesTrash.push(task);
   localStorage.setItem('to-do-list-Th', JSON.stringify(valuesTrash));
   
+  let trashBadge = document.getElementById('trash-badge'); // Obter o elemento do badge
+  trashBadge.textContent = valuesTrash.length; // Atualizar o valor do badge com a contagem de itens removidos
+  
   showValues();
 }
+
 // ==========================================================================================================================================
+
 function removeItemDef(data) {
   let valuesTrash = JSON.parse(localStorage.getItem(localStorageKeyTrash) || "[]");
   let index = valuesTrash.findIndex(x => x.name == data);
@@ -180,8 +242,9 @@ function removeItemDef(data) {
 function updateCheckbox(data, checked) {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
   let index = values.findIndex(x => x.name == data);
-  values[index]['checked'] = checked; // Atualiza o estado do checkbox no objeto de tarefa
-  localStorage.setItem(localStorageKey, JSON.stringify(values)); // Atualiza o localStorage com o novo estado do checkbox
+  values[index]['checked'] = checked;
+  localStorage.setItem(localStorageKey, JSON.stringify(values));
+  showValues();
 }
 function checkItem(data, checked){
 	let values =  JSON.parse(localStorage.getItem(localStorageKey) || "[]")
@@ -191,6 +254,10 @@ function checkItem(data, checked){
   localStorage.setItem(localStorageKey, JSON.stringify(values));
 }
 // ==========================================================================================================================================
-showValues()
+
 showValuesTrash()
 showValuesDone()
+showValues();
+
+
+
